2d97b691580e26a1bd88c2be55f5348f3932133d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-06-19 15:24:57 -0700

    minor style tweak

--- commands/app.js
+++ commands/app.js
@@ -5,8 +5,8 @@ let co  = require('co');
 
 function *app (context, heroku) {
   let res = yield {
-    app: heroku.apps(context.app).info(),
-    config: heroku.apps(context.app).configVars().info()
+    app:     heroku.apps(context.app).info(),
+    config:  heroku.apps(context.app).configVars().info()
   };
   cli.debug(res.app);
   cli.debug(res.config);
