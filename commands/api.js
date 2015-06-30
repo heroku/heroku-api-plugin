4ca17c7badb29b8ac0775991ae51a8cb9c362f4a
Mark Pundsack <mp@heroku.com>
2015-06-29 22:45:29 -0700

    Add api command

---
+++ commands/api.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let cli = require('heroku-cli-util');
+let fs  = require("co-fs");
+
+module.exports = {
+  topic: 'api',
+  description: 'make a single API request',
+  default: true,
+  help: `The api command is a convenient but low-level way to send requests
+to the Heroku API. It sends an HTTP request to the Heroku API
+using the given method on the given path. For methods PUT, PATCH,
+and POST, it uses stdin unmodified as the request body. It prints
+the response unmodified on stdout.
+
+Method name input will be upcased, so both 'heroku api GET /apps' and
+'heroku api get /apps' are valid commands.
+
+Examples:
+
+    $ heroku api GET /apps/myapp
+    { created_at: "2011-11-11T04:17:13-00:00",
+      id: "12345678-9abc-def0-1234-456789012345",
+      name: "myapp",
+      …
+    }
+
+    $ export HEROKU_HEADERS
+    $ HEROKU_HEADERS='
+    Content-Type: application/x-www-form-urlencoded
+    Accept: application/json
+    '
+    $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
+    2
+`,
+  needsApp: false,
+  needsAuth: true,
+  args: [
+    {name: 'method', description: 'GET, POST, PUT, PATCH, or DELETE', optional: false},
+    {name: 'path', description: 'endpoint to call', optional: false}
+  ],
+  flags: [
+    {name: 'version', char: 'v', description: 'version to use (e.g. 2, 3, or 3.variant)', hasValue: true}
+  ],
+  run: cli.command(function* (context, heroku) {
+    let request = {};
+    request.method = context.args.method.toUpperCase();
+    request.path = context.args.path;
+    let version = context.flags.version || "3";
+    request.headers = { 'Accept': `application/vnd.heroku+json; version=${version}` };
+    if (request.method == "PATCH" || request.method == "PUT" || request.method == "POST") {
+      request.body = yield fs.readFile('/dev/stdin', 'utf8');
+    }
+    let response = yield heroku.request(request);
+    cli.log(response);
+  })
+};
--- test/commands/api.js
+++ test/commands/api.js
@@ -5,7 +5,7 @@ let nock  = require('nock');
 let sinon = require('sinon');
 let cmd   = require('../../commands/app');
 
-describe('hello:app', function () {
+describe('api', function () {
   beforeEach(function () {
     this.cliDebug = sinon.stub(cli, 'debug');
   });
@@ -14,23 +14,19 @@ describe('hello:app', function () {
     this.cliDebug.restore();
   });
 
-  it('displays the app info and config vars', function () {
+  it('displays the app info', function () {
     let self   = this;
+    let method = "get";
+    let path = "/app/myapp";
     let app    = {name: 'myapp', web_url: 'https://myapp.herokuapp.com/'};
-    let config = {FOO: 'bar'};
 
     nock('https://api.heroku.com')
     .get('/apps/myapp')
     .reply(200, app);
 
-    nock('https://api.heroku.com')
-    .get('/apps/myapp/config-vars')
-    .reply(200, config);
-
-    return cmd.run({app: 'myapp'})
+    return cmd.run({method: method, path: path})
     .then(function () {
       self.cliDebug.should.have.been.calledWith(app);
-      self.cliDebug.should.have.been.calledWith(config);
     });
   });
 });
