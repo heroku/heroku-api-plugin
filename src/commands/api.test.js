cd0c7ef44016b62a688f91f84e8e4f9708c48184
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-07-07 13:14:13 -0700

    finished v6

--- src/commands/api.test.js
+++ src/commands/api.test.js
@@ -1,8 +1,79 @@
 // @flow
 
 import API from './api'
+import nock from 'nock'
 
-test('runs the command', async () => {
-  let cmd = await API.mock()
-  expect(cmd.out.stdout.output).toEqual('hello world!\n')
+nock.disableNetConnect()
+
+let api
+
+beforeEach(() => {
+  api = nock('https://api.heroku.com')
+})
+
+afterEach(() => {
+  api.done()
+})
+
+describe('404', () => {
+  beforeEach(() => {
+    api
+      .get('/uhoh')
+      .reply(404, 'uhoh!')
+  })
+
+  it('throws error', async () => {
+    expect.assertions(1)
+    try {
+      await API.mock('/uhoh')
+    } catch (err) {
+      expect(err.message).toEqual("HTTP: 404 /uhoh\n'uhoh!'")
+    }
+  })
+})
+
+describe('GET /apps', () => {
+  beforeEach(() => {
+    api
+      .get('/apps')
+      .reply(200, [{name: 'myapp'}])
+  })
+
+  it('GETs by default', async () => {
+    let cmd = await API.mock('/apps')
+    let app = JSON.parse(cmd.out.stdout.output)[0]
+    expect(app).toMatchObject({name: 'myapp'})
+  })
+
+  it('GETs', async () => {
+    let cmd = await API.mock('GET', '/apps')
+    let app = JSON.parse(cmd.out.stdout.output)[0]
+    expect(app).toMatchObject({name: 'myapp'})
+  })
+
+  it('adds leading slash', async () => {
+    let cmd = await API.mock('GET', 'apps')
+    let app = JSON.parse(cmd.out.stdout.output)[0]
+    expect(app).toMatchObject({name: 'myapp'})
+  })
+})
+
+describe('POST /apps', () => {
+  beforeEach(() => {
+    api
+      .post('/apps')
+      .reply(201, {name: 'myapp'})
+  })
+
+  it('POST', async () => {
+    // $FlowFixMe
+    process.stdin.isTTY = false
+    process.nextTick(() => {
+      process.stdin.push('{"FOO": "bar"}')
+      process.stdin.emit('end')
+    })
+    let cmd = await API.mock('POST', '/apps')
+    let app = JSON.parse(cmd.out.stdout.output)
+    expect(app).toMatchObject({name: 'myapp'})
+  })
 })
