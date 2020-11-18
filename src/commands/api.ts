cfc71afeb845da2a692c9a4ffee5d594910b2cae
Stanley Stuart <sstuart@salesforce.com>
2020-11-18 09:05:49 -0800

    chore: convert tslint to eslint / update packages / typescript

--- src/commands/api.ts
+++ src/commands/api.ts
@@ -3,7 +3,7 @@ import {Command, flags} from '@heroku-cli/command'
 import cli from 'cli-ux'
 import {HTTPRequestOptions} from 'http-call'
 import {inspect} from 'util'
-import { URL } from 'url'
+import {URL} from 'url'
 
 export default class API extends Command {
   static description = `make a manual API request
@@ -17,6 +17,7 @@ It is essentially like curl for the Heroku API.
 
 Method name input will be upcased, so both 'heroku api GET /apps' and
 'heroku api get /apps' are valid commands.`
+
   static examples = [`$ heroku api GET /apps/myapp
 {
   created_at: "2011-11-11T04:17:13-00:00",
@@ -42,11 +43,12 @@ $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
   static flags = {
     version: flags.string({char: 'v', description: 'version to use (e.g. 2, 3, or 3.variant)'}),
     'accept-inclusion': flags.string({char: 'a', description: 'Accept-Inclusion header to use'}),
-    body: flags.string({char: 'b', description: 'JSON input body'})
+    body: flags.string({char: 'b', description: 'JSON input body'}),
   }
+
   static args = [
     {name: 'method', description: 'GET, POST, PUT, PATCH, or DELETE', required: true},
-    {name: 'path', description: 'endpoint to call'}
+    {name: 'path', description: 'endpoint to call'},
   ]
 
   async run() {
@@ -55,7 +57,7 @@ $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
       const getStdin = require('get-stdin')
       const edit = require('edit-string')
 
-      let body = flags.body || await getStdin()
+      const body = flags.body || await getStdin()
       if (!body) {
         this.warn('no stdin provided')
         return
@@ -63,16 +65,17 @@ $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
       try {
         return JSON.parse(body)
       } catch {
-        let err = new Error(`Request body must be valid JSON
+        const err = new Error(`Request body must be valid JSON
   Received:
   ${inspect(body)}`)
         if (process.stdin.isTTY) {
           this.warn(err)
           return JSON.parse(await edit(body, {postfix: '.json'}))
-        } else throw err
+        }
+        throw err
       }
     }
-    let request: HTTPRequestOptions = {headers: {}}
+    const request: HTTPRequestOptions = {headers: {}}
     let path = args.path
     request.method = args.method
     if (!path) {
@@ -81,12 +84,13 @@ $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
     }
     request.method = request.method!.toUpperCase()
     const uri = this.parseURL(path)
-    let version = flags.version || '3'
+    const version = flags.version || '3'
     request.headers!.accept = `application/vnd.heroku+json; version=${version}`
     if (flags['accept-inclusion']) {
       request.headers!['Accept-Inclusion'] = flags['accept-inclusion']
     }
     if (request.method === 'PATCH' || request.method === 'PUT' || request.method === 'POST') {
+      // eslint-disable-next-line require-atomic-updates
       request.body = await getBody()
     }
     const fetch = async (body: any[] = []): Promise<string | any[]> => {
@@ -94,10 +98,10 @@ $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
       let response
       try {
         response = await this.heroku.request<any>(uri.toString(), request)
-      } catch (err) {
-        if (!err.http || !err.http.statusCode) throw err
-        cli.action.stop(color`{redBright ${err.http.statusCode}}`)
-        throw err
+      } catch (error) {
+        if (!error.http || !error.http.statusCode) throw error
+        cli.action.stop(color`{redBright ${error.http.statusCode}}`)
+        throw error
       }
       let msg = color`{greenBright ${response.response!.statusCode!.toString()}}`
       if (Array.isArray(response.body)) msg += ` ${response.body.length + body.length} items`
@@ -108,7 +112,7 @@ $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
       }
       return Array.isArray(response.body) ? body.concat(response.body) : response.body
     }
-    let body = await fetch()
+    const body = await fetch()
     if (typeof body === 'string') {
       cli.log(body)
     } else {
@@ -119,7 +123,7 @@ $ printf 'type=web&qty=2' | heroku api POST /apps/myapp/ps/scale
   private parseURL(path: string): URL {
     try {
       return new URL(path)
-    } catch(err) {
+    } catch (error) {
       const normalizedPath = path.startsWith('/') ? path : `/${path}`
       const protocol = this.heroku.defaults.protocol || 'https:'
       const normalizedURL = `${protocol}//${this.heroku.defaults.host!}${normalizedPath}`
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import path from 'path'
 
 export const topic = {
   name: 'api',
-  description: 'call the Heroku API directly'
+  description: 'call the Heroku API directly',
 }
 
-let dir = path.join(__dirname, 'commands')
+const dir = path.join(__dirname, 'commands')
 export const commands = fs.readdirSync(dir)
-  .filter(f => path.extname(f) === '.js')
-  // $FlowFixMe
-  .map(f => require('./commands/' + f).default)
+.filter(f => path.extname(f) === '.js')
+// $FlowFixMe
+.map(f => require('./commands/' + f).default)
