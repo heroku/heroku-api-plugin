d04e4803292919eeef9d081fe82dad095a124841
Santiago Bosio <santiago.bosio@gmail.com>
2026-03-04 14:25:43 -0300

    fix: address high security vulns reported by Dependabot (#162)

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import oclif from 'eslint-config-oclif'
+
+export default [
+  ...oclif,
+  {
+    ignores: [
+      './lib',
+      '**/*.js',
+    ],
+  },
+  {
+    files: [
+      '**/*.ts',
+    ],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          modules: true,
+        },
+        ecmaVersion: 6,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@stylistic/comma-dangle': 'warn',
+      '@stylistic/function-paren-newline': 'warn',
+      '@stylistic/indent': 'warn',
+      '@stylistic/lines-between-class-members': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'warn',
+      camelcase: 'off',
+      'import/namespace': 'warn',
+      'mocha/no-mocha-arrows': 'warn',
+      'n/shebang': 'warn',
+      'node/no-missing-import': 'off',
+      'perfectionist/sort-imports': 'warn',
+      'perfectionist/sort-objects': 'warn',
+      'prefer-arrow-callback': 'warn',
+      'unicorn/no-array-for-each': 'off',
+      'unicorn/no-useless-undefined': 'warn',
+      'unicorn/prefer-node-protocol': 'warn',
+      'unicorn/prefer-number-properties': 'warn',
+    },
+  },
+]
